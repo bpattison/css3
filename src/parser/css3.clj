c0690b21809d5dff3ca4df9291a8f7d50bdcb034
Brian Pattison <brian.pattison@gmail.com>
2009-09-18 13:40:25 -0400

    update README and licensing info

--- src/parser/css3.clj
+++ src/parser/css3.clj
@@ -1,5 +1,15 @@
+;; css3
+;;
+;; Copyright (c) 2009 Brian Pattison. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
 ;
-; based on joshua choi's JSON parser
+; based on joshua choi's FnParse and JSON parser
 ;
 (ns parser.css3
   (:use name.choi.joshua.fnparse
